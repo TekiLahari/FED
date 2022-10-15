379ce52f12149f9b3140460bf688f20be68d5d09
TekiLahari <96244862+TekiLahari@users.noreply.github.com>
2022-10-15 06:19:29 +0530

    Add files via upload
    first react commit

---
+++ App.js
@@ -0,0 +1,23 @@
+/*import './App.css';
+import CricScore from './CricScore';
+function App() {
+ return (
+ <div>
+ <CricScore />
+ </div>
+ );
+}
+export default App;
+*/
+import './App.css';
+import ChangeColors from './ChangeColors.js';
+function App() {
+ return (
+ <div style={{width:'90%', margin: '0 auto'}}>
+ <ChangeColors />
+ </div>
+ );
+}
+export default App;
+
+
